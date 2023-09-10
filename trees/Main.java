bf9f8a59a7eb395ac0216986d7b1baecb04ac4af
shivani283 <shivanivikhar283@gmail.com>
2023-09-11 01:18:29 +0530

    contains method - added

--- trees/Main.java
+++ trees/Main.java
@@ -7,6 +7,11 @@ public class Main {
         //inserting a node in Tree
         mTreeFunc.insert(10);
         mTreeFunc.insert(25);
+        mTreeFunc.insert(5);
+
+        //contains method
+        // mTreeFunc.contains(10);
+        System.out.println(mTreeFunc.contains(10));
 
         System.out.println("root "+ mTreeFunc.root.value);
     }
--- trees/TreeFunc.java
+++ trees/TreeFunc.java
@@ -43,4 +43,24 @@ public class TreeFunc {
             }
         }
     }
+
+    //contains method
+    public boolean contains(int value){
+        if(root==null){
+            return false;
+        }
+        Node temp = root;
+        while(temp!=null){
+            if(value < temp.value){
+                temp = temp.left;
+            }
+            else if(value > temp.value){
+                temp = temp.right;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
 }
