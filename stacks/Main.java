c2df768d3062dee82fb7c0ef461b852b11fbbfd2
shivani283 <shivanivikhar283@gmail.com>
2023-09-10 23:56:46 +0530

    stack operations -added

--- stacks/Main.java
+++ stacks/Main.java
@@ -2,6 +2,18 @@ package stacks;
 
 public class Main {
     public static void main(String[] args) {
-        StackFunc myStackFunc = new StackFunc(0);
+        StackFunc myStackFunc = new StackFunc(9);
+
+        //pushing into stack
+        myStackFunc.push(10);
+        myStackFunc.push(11);
+        myStackFunc.push(12);
+        myStackFunc.push(13);
+
+        //poping from stack
+        myStackFunc.pop();
+
+        //printing stack
+        myStackFunc.printStack();
     }
 }
--- stacks/StackFunc.java
+++ stacks/StackFunc.java
@@ -18,4 +18,55 @@ public class StackFunc {
         top = newNode;
         height = 1;
     }
+
+    //Push method in stacks
+    //condtion -- if full, cant push 
+    //full when top == height
+    //empty when height == 0
+    public void push(int value){
+        //creating a newNode;
+        Node newNode = new Node(value);//create node with the given value
+        if(height==0){
+            top = newNode;
+        }
+        else{
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+    //method to pop elements from stack
+    public Node pop(){
+        if(height==0){
+            System.out.println("cannot pop, stack empty");
+            return null;
+        }
+        else{
+            Node temp = top;
+            top = top.next;
+            temp.next = null;
+            height--;
+            return temp;
+        }
+    }
+
+    //method to print the stack;
+    public void printStack(){
+        Node temp = top;
+        while(temp!=null){
+            System.out.println(temp.value+" ");
+            temp = temp.next;
+        }
+    }
+
+    //method to get top of stack;
+    public Node getTop(){
+        return top;
+    }
+
+    //method to get Height of the stack
+    public int getHeight(){
+        return height;
+    }
 }
